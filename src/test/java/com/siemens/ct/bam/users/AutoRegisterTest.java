decd30d82e4fab799b49dd73cf54883ea391e151
CTBAV\Cosmin.Rosu <cosmin.rosu@ctbav.ro>
2018-05-17 11:10:55 +0300

    -AutoRegisteTest class only created, without the methods;

---
+++ src/test/java/com/siemens/ct/bam/users/AutoRegisterTest.java
@@ -0,0 +1,22 @@
+package com.siemens.ct.bam.users;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AutoRegisterTest {
+
+    @Test
+    public void registerUser() {
+    }
+
+    @Test
+    public void registerUserAlreadyExist()
+    {
+
+    }
+
+    @Test
+    public void existUser() {
+    }
+}
